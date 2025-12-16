73a44a6342430a0679c9f851d760492e33f9d08b
Asatidz <fahri.dwi10096@smk.belajar.id>
2025-12-16 23:50:26 +0700

    dashbnoard admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Pendaftar;
+use App\Models\Verifikasi;
 
 class DashboardController extends Controller
 {
-    public function index() {
-        return view('admin.dashboard');
+    public function index()
+    {
+        // STATISTIK PENDAFTAR
+        $totalPendaftar = Pendaftar::count();
+        // AMBIL DATA VERIFIKASI 
+$belumBayar = Pendaftar::whereDoesntHave('verifikasi')
+    ->orWhereHas('verifikasi', function ($q) {
+        $q->where('verifikasi_pembayaran', 'pending');
+    })
+    ->count();
+
+        $pendaftarTerbaru = Pendaftar::latest()
+            ->take(5)
+            ->get();
+
+        return view('admin.dashboard', compact(
+            'totalPendaftar',
+            'belumBayar',
+            'pendaftarTerbaru'
+        ));
     }
 }
